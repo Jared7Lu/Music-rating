743c56acd4fa56367b292409d6f2201c2d29acc5
Jared7Lu <109761254+Jared7Lu@users.noreply.github.com>
2023-02-13 19:31:21 -0800

    Layout of search Bar with Spotify

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-
 import React from 'react'
 import './App.css'
 import Search from './components/search';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, InputGroup, Button, Card, FormControl} from 'react-bootstrap';
+import {useState, useEffect} from 'react';
 
 function App() {
+  const [searchInput, setSearchInput] = useState("")
+
   return (
     <div className="App">
-      <Search />
+      <InputGroup>
+        <Search />
+        <FormControl placeholder="Discover" type="input" onKeyPress ={event => {
+          if (event.key == "Enter") {
+            console.log("pressed enter")
+          }
+        }}
+          onChange={event => setSearchInput(event.target.value)}>
+        </FormControl>
+
+      </InputGroup>
+      <Container>
+        
+      </Container>
     </div>
 
 
