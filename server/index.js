8f482f13812ad3e3cb784b4b593b8bebc92d779e
Kenny K <koablood@gmail.com>
2023-02-15 18:26:54 -0800

    Attached to database

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
+// switched over from server/index.js to server/server.js
 // server /index.js
-const path = require("path"); // ***
-const express = require("express");
-const PORT = process.env.PORT || 3001;
+// const path = require("path"); // ***
+// const express = require("express");
+// const PORT = process.env.PORT || 3001;
 
-const app = express();
+// const app = express();
 
-// API Endpoint
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-});
+// // API Endpoint
+// app.get("/api", (req, res) => {
+//     res.json({ message: "Hello from server!" });
+// });
 
-app.listen(PORT, () => {
-    console.log(`Server listing on ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server listing on ${PORT}`);
+// });
--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
-// Entry point the core of server
+// server /index.js
+const path = require("path"); // ***
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// API Endpoint
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listing on ${PORT}`);
+});
