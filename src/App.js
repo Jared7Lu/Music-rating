25d5aa4535b0946173aa293bef26bbc792712f18
Jared7Lu <109761254+Jared7Lu@users.noreply.github.com>
2023-02-15 19:51:44 -0800

    Spotify and search bar linked

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css'
 import Search from './components/search';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, InputGroup, Card, FormControl, Row} from 'react-bootstrap';
+import { Container, InputGroup, Card, FormControl, Row, Button} from 'react-bootstrap';
 import {useState, useEffect} from 'react';
 
 const CLIENT_ID="0474c79b015843b9b4e9a4b67cbe80c7";
@@ -11,6 +11,7 @@ const CLIENT_SECRET="f5aa899ec05f4ce6bcee4753337bca46";
 function App() {
   const [searchInput, setSearchInput] = useState("")
   const [accessToken, setAccessToken] = useState("data")
+  const [albums, setAlbums] = useState([]);
 
   //How to get token to get Spotify Data
   useEffect(() => {
@@ -26,27 +27,61 @@ function App() {
     .then(data => setAccessToken(data.access_token))
   }, [])
 
+  //Search Bar
+  async function search() {
+    console.log("You are Discovering " + searchInput);
+
+    //Get data from Spotify
+    var searchPara = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + accessToken
+      }
+    }
+
+    var artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', searchPara)
+    .then(response => response.json())
+    .then(data => { return data.artists.items[0].id })
+
+    console.log("artist id is" + artistID);
+
+    //get artist
+    var returnedAlbums = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/albums' + '?include_groups=album&market=US&limit=50', searchPara)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      setAlbums(data.items);
+    })
+console.log(albums);
+    //What to show from data
+  }
   return (
     <div className="App">
       <Container>
         <InputGroup>
-            <FormControl placeholder="Discover" type="input" onKeyPress ={event => {
+            <FormControl placeholder="Discover" type="input" 
+            onKeyPress ={event => {
               if (event.key == "Enter") {
-              console.log("pressed enter")
+              search();
               }
             }}
             onChange={event => setSearchInput(event.target.value)}/>
-            <button> you</button>       
+            <Button onClick={search}>Discover</Button>      
         </InputGroup>
       </Container>
       <Container>
         <Row className="mx-2 row row-cols-5">
-          <Card>
-            <Card.Img />
+          {albums.map((album, i) => {
+            return(
+              <Card>
+            <Card.Img src={album.images[0].url}/>
             <Card.Body>
-              <Card.Title>Sample</Card.Title>
+              <Card.Title>{album.name}</Card.Title>
             </Card.Body>
           </Card>
+            )
+          })}
         </Row>
       </Container>
     </div>
