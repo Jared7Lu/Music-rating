57c72b779f1dca295a6281e4dea818004135d6f0
torian austin <torian03@outlook.com>
2023-02-16 08:17:08 -0800

    Finish comments component

---
+++ src/components/Comment.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+
+function Comment(){
+  return (
+  <div>
+    <Form>
+      <Row>
+        <Col className="col-md-2">
+        <FloatingLabel controlId="floatingInput" label="UserName" className="mb-3">
+          <Form.Control placeholder="UserName" />
+        </FloatingLabel>
+        </Col>
+        <Col className="col-md-7">
+        <FloatingLabel controlId="floatingInput" label="Comment">
+          <Form.Control placeholder="Comment" />
+        </FloatingLabel>
+        </Col>
+        <Col className='col-md-1'>
+          <input type="submit" className="btn btn-primary" value="Add Comment"/>
+        </Col>
+      </Row>
+    </Form>
+  </div>
+  )
+}
+
+export default Comment 
